ffe132ee544b28d8ce639dbaa3726abfb63a3ab1
linda <zz3130@nyu.edu>
2025-12-05 09:46:37 -0800

    evaluate model performance using RSS in log space, which follows Jan's JoV2023 paper; update the dev notebook

--- src/corticalcrowding/regression.py
+++ src/corticalcrowding/regression.py
@@ -5,14 +5,17 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 
-def fit_and_evaluate(X, Y):
+def fit_and_evaluate(X, Y, logerror=True):
     # set intercept = 0
     model = LinearRegression(fit_intercept = False)
     X = X.reshape(-1, 1)
     model.fit(X, Y)
     y_pred = model.predict(X)
     # residual sum of squares
-    rss = np.sum((Y - y_pred) ** 2)
+    if logerror:
+        rss = np.sum((np.log10(Y) - np.log10(y_pred)) ** 2)
+    else:
+        rss = np.sum((Y - y_pred) ** 2)
     return rss, model.coef_[0]
 
 
