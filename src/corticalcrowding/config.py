ff19fabbf412fe0367a4783673f28d2bbef7510a
linda <zz3130@nyu.edu>
2026-05-05 09:01:01 -0700

    update notebook with more comments

--- src/corticalcrowding/config.py
+++ src/corticalcrowding/config.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 
 #NEI_proc_path = Path('/data/NYU-NEI/')
-NEI_proc_path = Path('/data/crowding/BIDS/derivatives/')
-NYU_proc_path = Path('/data/openneuro/ds003787/derivatives/')
+#NEI_proc_path = Path('/data/crowding/BIDS/derivatives/')
+#NYU_proc_path = Path('/data/openneuro/ds003787/derivatives/')
+
+NEI_proc_path = Path('/data/crowding/NEI-BIDS/derivatives')
+NYU_proc_path = Path('/data/crowding/NYU-BIDS/derivatives')
+
