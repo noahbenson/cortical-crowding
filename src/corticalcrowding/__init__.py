698e1a9909a162a874fd5bc27b2c563e3c27b3f0
Noah C. Benson <nben@uw.edu>
2026-06-16 08:24:33 -0700

    Update __init__.py
    Cleanup imports

--- src/corticalcrowding/__init__.py
+++ src/corticalcrowding/__init__.py
@@ -2,21 +2,15 @@
 crowding distances in the visual field and cortical magnification.
 """
 
-from .cmag import load_subject
-from .cmag import HH91, HH91_integral, HH91_gain
-from .cmag import HH91_fit, HH91_fit_cumarea, cmag_basics, fit_cumarea, signed_bounds_from_abs_ranking
-from .cmag import ring_area_deg2
-from .cmag import ring_cmag
-from .cmag import bilateral_areal_cmag
-from .cmag import calculate_cortical_magnification
+from .cmag import (
+    load_subject,
+    HH91, HH91_integral, HH91_gain, HH91_fit, HH91_fit_cumarea,
+    cmag_basics,
+    fit_cumarea,
+    signed_bounds_from_abs_ranking,
+    ring_area_deg2,
+    ring_cmag)
 
-from .regression import fit_and_evaluate
-
-from .crowding import Kurzawski2023_cd, log_Kurzawski2023_cd
-from .crowding import bootstrap_fit
-
-from .corticalcrowding import bootstrap_cv
-
-
-
-
+from .crowding import (
+    Kurzawski2023_cd, log_Kurzawski2023_cd,
+    bootstrap_fit, bootstrap_cv)
