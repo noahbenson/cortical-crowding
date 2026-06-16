a84a18937fdb18811bd35b80ff897b28907fa32e
Noah C. Benson <n@nben.net>
2026-06-16 08:51:22 -0700

    updating notebook and removing invsuplin_gain reference

--- src/corticalcrowding/__init__.py
+++ src/corticalcrowding/__init__.py
@@ -5,7 +5,7 @@ crowding distances in the visual field and cortical magnification.
 from .cmag import (
     load_subject,
     HH91, HH91_integral, HH91_gain, HH91_fit, HH91_fit_cumarea,
-    invsuplin, invsuplin_integral, invsuplin_gain, invsuplin_fit, invsuplin_fit_cumarea,
+    invsuplin, invsuplin_integral, invsuplin_fit, invsuplin_fit_cumarea,
     cmag_basics,
     fit_cumarea,
     signed_bounds_from_abs_ranking,
@@ -15,3 +15,5 @@ from .cmag import (
 from .crowding import (
     Kurzawski2023_cd, log_Kurzawski2023_cd,
     bootstrap_fit, bootstrap_cv)
+
+from . import config
